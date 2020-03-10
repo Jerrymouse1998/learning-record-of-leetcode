df9ff61825b27797c2b17fb24b60e453781cbbba
jerrymouse1998 <577373789@qq.com>
2020-03-10 20:37:59 +0800

    Definition for a binary tree node

--- src/DataStruction/TreeNode.java
+++ src/DataStruction/TreeNode.java
@@ -2,8 +2,8 @@ package DataStruction;
 
 //简单的二叉树
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
 }
