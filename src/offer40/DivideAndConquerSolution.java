6977d24201b065d1c0de348360fefd02330634f0
jerrymouse1998 <577373789@qq.com>
2020-03-21 16:12:38 +0800

    offer40

--- src/offer40/DivideAndConquerSolution.java
+++ src/offer40/DivideAndConquerSolution.java
@@ -1,73 +1,46 @@
 package offer40;
 
 public class DivideAndConquerSolution {
-    //时间复杂度：O(n^2)  平均时间复杂度：O(n)
+    //时间复杂度的分析方法和快速排序类似。由于快速选择只需要递归一边的数组，时间复杂度小于快速排序。
+    // 期望时间复杂度为O(n)，最坏情况下的时间复杂度为O(n^2)
     public int[] getLeastNumbers(int[] arr, int k) {
-        if (k == 0) {
-            return new int[0];
-        } else if (arr.length <= k) {
-            return arr;
-        }
-
-        // 原地不断划分数组
-        partitionArray(arr, 0, arr.length - 1, k);
-
-        // 数组的前 k 个数此时就是最小的 k 个数，将其存入结果
+        if (k == 0) return new int[0];
+        if (arr.length <= k) return arr;
+        //不断划分数组并调整
+        partitionArray(arr, 0, arr.length-1, k);
+        //经过划分调整之后数组前k个数已经是最小的k个
         int[] res = new int[k];
         for (int i = 0; i < k; i++) {
-            res[i] = arr[i];
+            res[i]=arr[i];
         }
         return res;
     }
 
-    void partitionArray(int[] arr, int lo, int hi, int k) {
-        // 做一次 partition 操作
-        int m = partition(arr, lo, hi);
-        // 此时数组前 m 个数，就是最小的 m 个数
-        if (k == m) {
-            // 正好找到最小的 k(m) 个数
-            return;
-        } else if (k < m) {
-            // 最小的 k 个数一定在前 m 个数中，递归划分
-            partitionArray(arr, lo, m-1, k);
-        } else {
-            // 在右侧数组中寻找最小的 k-m 个数
-            partitionArray(arr, m+1, hi, k);
+    private void partitionArray(int[] arr, int low, int high, int k) {
+        //和快排的partition一样
+        if (low<high) {
+            int idx = partition(arr, low, high);
+            //idx==k，idx左侧部分是最小TopK
+            if (idx == k) {
+                return;
+            } else if (k < idx) {
+                partitionArray(arr, low, idx - 1, k);
+            } else {
+                partitionArray(arr, idx + 1, high, k);
+            }
         }
     }
-
-    // partition 函数和快速排序中相同，具体可参考快速排序相关的资料
-    // 代码参考 Sedgewick 的《算法4》
-    int partition(int[] a, int lo, int hi) {
-        int i = lo;
-        int j = hi + 1;
-        int v = a[lo];
-        while (true) {
-            while (a[++i] < v) {
-                if (i == hi) {
-                    break;
-                }
-            }
-            while (a[--j] > v) {
-                if (j == lo) {
-                    break;
-                }
-            }
-
-            if (i >= j) {
-                break;
-            }
-            swap(a, i, j);
+    //partitoin操作和快排一样
+    private int partition(int[] arr, int low, int high) {
+        int l=low,h=high,temp=arr[low];
+        while (l < h) {
+            while (l<h&&arr[h]>=temp)h--;
+            if (l<h)arr[l++]=arr[h];
+            while (l<h&&arr[l]<=temp)l++;
+            if (l<h)arr[h--]=arr[l];
         }
-        swap(a, lo, j);
-
-        // a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
-        return j;
+        arr[l]=temp;
+        return l;
     }
 
-    void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
 }
---
+++ src/sort/QuickSort.java
@@ -0,0 +1,35 @@
+package sort;
+
+public class QuickSort {
+
+    /*
+        平均/最好时间复杂度：O(nlogn) 每次分割对基准数排序O(n)，递归深度logn
+        最坏退化到：O(n^2)
+        不稳定
+     */
+    public static void quickSort(int[] arr, int low, int high) {
+        //递归结束条件low==right
+        if (low < high) {
+            int l = low, h = high, temp = arr[low];
+            while (l < h) {
+                //逆序找到第一个小于temp的，放到l坑位
+                while (l < h && arr[h] >= temp) h--;
+                if (l < h) {
+                    arr[l] = arr[h];
+                    l++;
+                }
+                //顺序找到第一个大于temp，放到r坑位
+                while (l < h && arr[l] <= temp) l++;
+                if (l < h) {
+                    arr[h] = arr[l];
+                    h--;
+                }
+            }
+            //基准数放到最终l==r的坑位
+            arr[l] = temp;
+            //递归基准数左右部分
+            quickSort(arr, low, l - 1);
+            quickSort(arr, l + 1, high);
+        }
+    }
+}
