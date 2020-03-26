0340dfd67d2539d50640ffa6ed105d6020055df7
jerrymouse1998 <577373789@qq.com>
2020-03-26 16:35:43 +0800

    sort

--- src/sort/Test.java
+++ src/sort/Test.java
@@ -32,13 +32,13 @@ public class Test {
         BucketSort.BucketSort(arr,100);
         long end = System.currentTimeMillis();
 
-        //System.out.print("排序后:[");
-        //for (int i = 0; i < arr.length; i++) {
-        //    System.out.print(arr[i]);
-        //    if (i < arr.length - 1) System.out.print(",");
-        //}
-        //System.out.println("]");
-        //
+        System.out.print("排序后:[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(",");
+        }
+        System.out.println("]");
+
         System.out.println("排序耗时：" + (end - start) + "ms");
     }
 }
