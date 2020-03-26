df1ef0117176a0ad247e51d5317b61ba8b46ed45
jerrymouse1998 <577373789@qq.com>
2020-03-26 16:22:32 +0800

    sort

---
+++ src/sort/CountingSort.java
@@ -0,0 +1,35 @@
+package sort;
+
+public class CountingSort {
+    /*
+        计数排序
+        平均/最好/最坏时间复杂度：O(n+k)    n是arr长度，k是范围
+        空间复杂度：O(k)
+        稳定
+     */
+    public static void countingSort(int[] arr){
+        if (arr==null||arr.length<2)return;
+        //找到数组中最大、最小，以及两者之间的偏差值
+        int min=arr[0],max=arr[0],bias=0;
+        for (int e : arr) {
+            if (e>max)max=e;
+            if (e<min)min=e;
+        }
+        bias=0-min;
+        //计数
+        int[] counter=new int[max-min+1];
+        for (int e : arr) {
+            counter[e+bias]++;
+        }
+        //将计数器中的元素依次取出，即可完成排序
+        int counter_idx=0,arr_idx=0;
+        while (counter_idx < counter.length) {
+            if (counter[counter_idx] != 0) {
+                counter[counter_idx]--;
+                arr[arr_idx++]=counter_idx-bias;
+            }else {
+                counter_idx++;
+            }
+        }
+    }
+}
--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -7,15 +7,50 @@ public class HeapSort {
         空间复杂度：O(1)
         不稳定
      */
-    public static void heapSort(int[] arr){
 
+    //无序区的长度
+    static int len;
+
+    public static void heapSort(int[] arr) {
+        len = arr.length;
+        if (len < 2) return;
+        //构建大顶堆
+        buildHeap(arr);
+        while (len > 0) {
+            //将堆顶放到最后一个元素，找到最大值
+            swap(arr, 0, len - 1);
+            //缩小无序区，对无序区重新调整为大顶堆
+            len--;
+            adjustHeap(arr, 0);
+        }
     }
 
-    public static void buildHeap(int[] arr){
-
+    public static void buildHeap(int[] arr) {
+        //从最后一个非叶子节点逐步向上构建大顶堆
+        for (int i = len / 2 - 1; i >= 0; i--) {
+            adjustHeap(arr, i);
+        }
     }
 
-    public static void adjustHeap(int[] arr,int i){
+    public static void adjustHeap(int[] arr, int i) {
+        int maxIdx = i;
+        //找到左右子树中大于父节点的最大的元素的下标
+        if (i * 2 + 1 < len && arr[i * 2 + 1] > arr[maxIdx]) {
+            maxIdx = i * 2 + 1;
+        }
+        if (i * 2 + 2 < len && arr[i * 2 + 2] > arr[maxIdx]) {
+            maxIdx = i * 2 + 2;
+        }
+        //如果父节点不是最大值，交换父节点和左右子树最大值的位置，并继续递归调整移动之后的父节点
+        if (maxIdx != i) {
+            swap(arr, i, maxIdx);
+            adjustHeap(arr, maxIdx);
+        }
+    }
 
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
