9e336d3c181f554b082cd357fba3dde9b6b8579b
jerrymouse1998 <577373789@qq.com>
2020-04-11 15:54:03 +0800

    question102

--- src/question102/BFSSolution.java
+++ src/question102/BFSSolution.java
@@ -7,21 +7,21 @@ import java.util.*;
 public class BFSSolution {
     //时间复杂度：O(n)
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res=new ArrayList<>();
-        if (root==null)return res;
-        Queue<TreeNode> queue=new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         //层序遍历
         while (!queue.isEmpty()) {
             //记录一层的数值
-            List<Integer> round=new ArrayList<>();
+            List<Integer> round = new ArrayList<>();
             int size = queue.size();
             //弹出一层节点
             for (int i = 0; i < size; i++) {
                 TreeNode p = queue.poll();
                 round.add(p.val);
-                if (p.left!=null)queue.add(p.left);
-                if (p.right!=null)queue.add(p.right);
+                if (p.left != null) queue.add(p.left);
+                if (p.right != null) queue.add(p.right);
             }
             res.add(round);
         }
