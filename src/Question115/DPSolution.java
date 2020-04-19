86efa2935b53ae3152824c14eb105eac381fd0f9
jerrymouse1998 <577373789@qq.com>
2020-04-19 16:08:44 +0800

    question116

---
+++ src/Question115/DPSolution.java
@@ -0,0 +1,20 @@
+package Question115;
+
+public class DPSolution {
+    //时间复杂度：O(m*n)
+    public int numDistinct(String s, String t) {
+        //初始化
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i < s.length() + 1; i++) {
+            dp[i][0] = 1;
+        }
+        //状态转移
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 1; j < t.length() + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+}
---
+++ src/question116/Solution.java
@@ -0,0 +1,26 @@
+package question116;
+
+import DataStruction.Node;
+
+public class Solution {
+    //时间复杂度：O(n)
+    public Node connect(Node root) {
+        if (root == null) return root;
+        Node p = root;
+        //p指向每一层的最左节点
+        while (p.left != null) {
+            Node temp = p;
+            while (temp != null) {
+                //第一种情况
+                temp.left.next = temp.right;
+                //第二种情况
+                if (temp.next != null) {
+                    temp.right.next = temp.next.left;
+                }
+                temp = temp.next;
+            }
+            p = p.left;
+        }
+        return root;
+    }
+}
